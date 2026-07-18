970df17d41aad4e82c2ab1ef47db5bcc4a23d26d
AndoNando15 <andonando158@gmail.com>
2026-07-18 19:49:50 +0700

    config: set halaman awal redirect ke /daftar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/daftar',
+        permanent: true, // true untuk SEO dan redirect permanen (301)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
